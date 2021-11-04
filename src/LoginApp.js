98e3ea46968167170e34cec07d6d90be8f88bc22
Cristian Daniel Morales Villada <cristian980829@gmail.com>
2021-11-04 18:12:11 -0500

    boostrap implemented

--- src/LoginApp.js
+++ src/LoginApp.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const LoginApp = () => {
     return (
-        <>
+        <div className="container mt-3">
             <h2>Login App</h2>
-        </>
+        </div>
     )
 }
