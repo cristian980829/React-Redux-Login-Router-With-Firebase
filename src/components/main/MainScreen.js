52d1914e08b468a07d6b842b49ec1596e76d90a4
Cristian Daniel Morales Villada <cristian980829@gmail.com>
2021-11-05 15:45:38 -0500

    PUBLIC AND PRIVATE ROUTES, MAIN SCREEN IMPLEMENTED

---
+++ src/components/main/MainScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MainScreen = () => {
+    return (
+        <div>
+            <h1>Main screen</h1>
+
+           
+        </div>
+    )
+}
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Redirect,
-    Route
-  } from 'react-router-dom';
+    Redirect
+} from 'react-router-dom';
 
-import { LoginScreen } from '../components/auth/LoginScreen';
-import { RegisterScreen } from '../components/auth/RegisterScreen';
+import { useDispatch } from 'react-redux';
+
+import { firebase } from '../firebase/firebase-config'
+
+import { login } from '../actions/auth';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
+import { AuthRouter } from './AuthRouter';
+import { MainScreen } from '../components/main/MainScreen';
+
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    const [ checking, setChecking ] = useState(true);
+    const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+
+
+
+    useEffect(() => {
+        
+        firebase.auth().onAuthStateChanged( async(user) => {
+
+            if ( user?.uid ) {
+                dispatch( login( user.uid, user.displayName ) );
+                setIsLoggedIn( true );
+
+            } else {
+                setIsLoggedIn( false );
+            }
+
+            setChecking(false);
+
+        });
+        
+    }, [ dispatch, setChecking, setIsLoggedIn ])
+
+
+    if ( checking ) {
+        return (
+            <h1>Wait...</h1>
+        )
+    }
+
+
+
+
     return (
         <Router>
             <div className="row">
@@ -18,16 +61,17 @@ export const AppRouter = () => {
                         <div className="card">
                             <div className="card-body text-center">
                                 <Switch>
-                                    <Route 
-                                        exact
-                                        path="/auth/login"
-                                        component={ LoginScreen }
+                                    <PublicRoute 
+                                        path="/auth"
+                                        component={ AuthRouter }
+                                        isAuthenticated={ isLoggedIn }
                                     />
 
-                                    <Route 
+                                    <PrivateRoute 
                                         exact
-                                        path="/auth/register"
-                                        component={ RegisterScreen }
+                                        isAuthenticated={ isLoggedIn }
+                                        path="/"
+                                        component={ MainScreen }
                                     />
                                     
 
